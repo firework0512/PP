492ec69319b6c08c8c08cb6b985f6129e41b5407
luna <123723119@qq.com>
2020-03-09 10:37:07 +0100

    Solucionado el bug del modo

--- elTercerClon.py
+++ elTercerClon.py
@@ -2,21 +2,20 @@
 import random
 import os.path
 
-global mod
 mod = 1
-nivel = range (1, 12)
+nivel = range(1, 12)
 
-print ("‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐  CLON‐3  ‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐")
-print ("‐ Práctica de Paradigmas de Programación 2019‐20 ‐")
-print ("‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐ ")
+print("‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐  CLON‐3  ‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐")
+print("‐ Práctica de Paradigmas de Programación 2019‐20 ‐")
+print("‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐‐ ")
 
 opciones = ("CREAR NUEVO TABLERO", "LEER TABLERO DE FICHERO", "SALIR")
 jugadas = ("Alfabeto", "Nivel", "1024", "2048")
 # Impresión de opciones
-for i in range (len (opciones)):
-    print ("%d. %s" % (i + 1, opciones[i]))
+for i in range(len(opciones)):
+    print("%d. %s" % (i + 1, opciones[i]))
 
-opcion = int (input ("Indique opción : "))
+opcion = int(input("Indique opción : "))
 
 #   Tamaño de la matriz
 #   Debe ser cuadrada
@@ -41,52 +40,52 @@ def numeroaletorio(origin, bound, step=1):
     :param step: los saltos de numero
     :return: el número aleatorio
     """
-    return random.randrange (origin, bound, step)
+    return random.randrange(origin, bound, step)
 
 
 def imprimirtablero(matrix):
-
-    Tablero (matrix)
-
-    print ("MOVIMIENTOS = %s |   PUNTUACIÓN = %s" % (str (movimientos), str (puntuacion)))
+    tablero(matrix)
+    print("MOVIMIENTOS = %s |   PUNTUACIÓN = %s" % (str(movimientos), str(puntuacion)))
     jugada = ""
     # Entrada de jugadas
     while jugada == "":
-        jugada = input ("[S]ubir, [B]ajar, [I]zda, [D]cha | [M]odo, [G]uardar, [F]in: ")
-        if (validajugada (jugada) == False):
-            print ("Entrada no valida")
-            jugada == ""
-    Jugadas (jugada)
+        jugada = input("[S]ubir, [B]ajar, [I]zda, [D]cha | [M]odo, [G]uardar, [F]in: ")
+        if not validajugada(jugada):
+            print("Entrada no válida")
+            jugada = ""
+    Jugadas(jugada)
     return None
 
 
 # Jugadas del tablero
 def subir():
-    print ("elegiste subir")
+    print("elegiste subir")
 
 
 def bajar():
-    print ("elegiste bajar")
+    print("elegiste bajar")
 
 
 def izda():
-    print ("elegiste izquierda")
+    print("elegiste izquierda")
 
 
 def dcha():
-    print ("elegiste derecha")
+    print("elegiste derecha")
 
 
 def modo():
     # return "elegiste modo"
-    print ("ESCOJA MODO DE VISUALIZACION: \n")
-    for i in range (len (jugadas)):
-        print ("%d. %s" % (i + 1, jugadas[i]))
-    mod = int (input ("\n Escoja opcion: "))
+    print("ESCOJA MODO DE VISUALIZACION: \n")
+    for i in range(len(jugadas)):
+        print("%d. %s" % (i + 1, jugadas[i]))
+    global mod
+    mod = int(input("\n Escoja opcion: "))
+    return None
 
 
 def guardar():
-    print ("elegiste guardar")
+    print("elegiste guardar")
     fichero = open(input("Indique la ruta de guardado: "), "a")
     fichero.write("\n", str(movimientos),
                   "\n", str(puntuacion),
@@ -99,8 +98,7 @@ def fin():
 
 
 def Jugadas(jugad):
-    jugad = jugad.upper ()
-    funcion = " "
+    jugad = jugad.upper()
     opciones = {
         "S": subir,
         "B": bajar,
@@ -110,82 +108,81 @@ def Jugadas(jugad):
         "G": guardar,
         "F": fin
     }
-    funcion = opciones.get (jugad, "No existe")
-    funcion ()
+    opciones.get(jugad, "No existe")()
 
 
 # Modos de juego
 def Tab1(matrix):
-    for fila in range (len (matrix)):
-        print ("+-" * n + "+")
-        for columna in range (len (matrix[0])):
+    for fila in range(len(matrix)):
+        print("+-" * n + "+")
+        for columna in range(len(matrix[0])):
             if columna == 0:
-                print ("|%s" % str (matriz[fila][columna]), end="|")
+                print("|%s" % str(matriz[fila][columna]), end="|")
             elif columna == n - 1:
-                print ("%s|" % str (matriz[fila][columna]))
+                print("%s|" % str(matriz[fila][columna]))
             else:
-                print (str (matriz[fila][columna]), end="|")
+                print(str(matriz[fila][columna]), end="|")
         if fila == n - 1:
-            print ("+-" * n + "+")
+            print("+-" * n + "+")
 
 
 def Tab2(matrix):
-    for fila in range (len (matrix)):
-        print ("+--" * n + "+")
-        for columna in range (len (matrix[0])):
+    for fila in range(len(matrix)):
+        print("+--" * n + "+")
+        for columna in range(len(matrix[0])):
             if columna == 0:
-                print ("|%s" % str (matriz[fila][columna]), end="")
-                print ("%s" % str (matriz[fila][columna]), end="|")
+                print("|%s" % str(matriz[fila][columna]), end="")
+                print("%s" % str(matriz[fila][columna]), end="|")
             elif columna == n - 1:
-                print ("%s" % str (matriz[fila][columna]), end="")
-                print ("%s|" % str (matriz[fila][columna]))
+                print("%s" % str(matriz[fila][columna]), end="")
+                print("%s|" % str(matriz[fila][columna]))
             else:
-                print (str (matriz[fila][columna]), end="")
-                print (str (matriz[fila][columna]), end="|")
+                print(str(matriz[fila][columna]), end="")
+                print(str(matriz[fila][columna]), end="|")
         if fila == n - 1:
-            print ("+--" * n + "+")
+            print("+--" * n + "+")
 
 
 def Tab3(matrix):
-    for fila in range (len (matrix)):
-        print ("+----" * n + "+")
-        for columna in range (len (matrix[0])):
+    for fila in range(len(matrix)):
+        print("+----" * n + "+")
+        for columna in range(len(matrix[0])):
             if columna == 0:
-                print ("|%s" % str (matriz[fila][columna]), end="")
-                print ("%s" % str (matriz[fila][columna]), end="")
-                print ("%s" % str (matriz[fila][columna]), end="")
-                print ("%s" % str (matriz[fila][columna]), end="|")
+                print("|%s" % str(matriz[fila][columna]), end="")
+                print("%s" % str(matriz[fila][columna]), end="")
+                print("%s" % str(matriz[fila][columna]), end="")
+                print("%s" % str(matriz[fila][columna]), end="|")
             elif columna == n - 1:
-                print ("%s" % str (matriz[fila][columna]), end="")
-                print ("%s" % str (matriz[fila][columna]), end="")
-                print ("%s" % str (matriz[fila][columna]), end="")
-                print ("%s|" % str (matriz[fila][columna]))
+                print("%s" % str(matriz[fila][columna]), end="")
+                print("%s" % str(matriz[fila][columna]), end="")
+                print("%s" % str(matriz[fila][columna]), end="")
+                print("%s|" % str(matriz[fila][columna]))
             else:
-                print (str (matriz[fila][columna]), end="")
-                print (str (matriz[fila][columna]), end="")
-                print (str (matriz[fila][columna]), end="")
-                print (str (matriz[fila][columna]), end="|")
+                print(str(matriz[fila][columna]), end="")
+                print(str(matriz[fila][columna]), end="")
+                print(str(matriz[fila][columna]), end="")
+                print(str(matriz[fila][columna]), end="|")
         if fila == n - 1:
-            print ("+----" * n + "+")
+            print("+----" * n + "+")
 
 
-def Tablero(matrix):
+def tablero(matrix):
     Tabl = {
         1: Tab1,
         2: Tab2,
         3: Tab3,
         4: Tab3
     }
-    func = Tabl.get (mod, "Ese modo no existe")
-    func (matrix)
+    func = Tabl.get(mod, "Ese modo no existe")
+    func(matrix)
 
 
 # Validar la entrada
 def validajugada(jug):
-    jug = jug.upper ()
+    jug = jug.upper()
     lista = ["S", "B", "I", "D", "M", "G", "F"]
     i = 0
-    for i in range (len (lista)):
+    for i in range(len(lista)):
         if jug == lista[i]:
             return True
         i += 1
@@ -195,8 +192,8 @@ def validajugada(jug):
 def crearobstaculos(m):
     obstaculos = 0
     while True:
-        aleatoriofila = numeroaletorio (0, n)
-        aleatoriocolumna = numeroaletorio (0, n)
+        aleatoriofila = numeroaletorio(0, n)
+        aleatoriocolumna = numeroaletorio(0, n)
         elemento = matriz[aleatoriofila][aleatoriocolumna]
         if elemento != "*":
             matriz[aleatoriofila][aleatoriocolumna] = "*"
@@ -211,42 +208,45 @@ def obstaculosdespuejugada():
     probabilidad[2] = 0.25
     return None
 
+
 def posiblejugada(matriz):
     for i in range(len(matriz)):
         for j in range(len(matriz[0])):
-            if matriz[i][j] ==" ":
+            if matriz[i][j] == " ":
                 return True
             j += 1
         i += 1
     return False
 
+
 # Contenido del archivo leido
 contenido = ""
+
 if opcion == 1:
-    n = int (input ("Introduzca el tamaño de la matríz: "))
-    m = int (input ("Introduzca el número de obstáculos: "))
+    n = int(input("Introduzca el tamaño de la matríz: "))
+    m = int(input("Introduzca el número de obstáculos: "))
     # Creamos el tablero del juego
-    matriz = [[" "] * n for i in range (n)]
+    matriz = [[" "] * n for i in range(n)]
     # Creación de obstaculos
-    crearobstaculos (m)
+    crearobstaculos(m)
     # Imprimimos el tablero
-    imprimirtablero (matriz)
-    enter = input ("Pulse cualquier tecla para mostrar inserción del nuevo bloque") == ""
+    imprimirtablero(matriz)
+    enter = input("Pulse cualquier tecla para mostrar inserción del nuevo bloque") == ""
     while posiblejugada(matriz):
-        imprimirtablero (matriz)
-        enter = input ("Pulse cualquier tecla para mostrar inserción del nuevo bloque") == ""
+        imprimirtablero(matriz)
+        enter = input("Pulse cualquier tecla para mostrar inserción del nuevo bloque") == ""
     print("HAS PERDIDO")
 elif opcion == 2:
     while True:
-        ruta = input ("Dame la ruta del archivo")
-        if os.path.isfile (ruta):
+        ruta = input("Dame la ruta del archivo")
+        if os.path.isfile(ruta):
             break
-    fichero = open (ruta, "r")
-    contenido = fichero.read ()
-    fichero.flush ()
-    fichero.close ()
+    fichero = open(ruta, "r")
+    contenido = fichero.read()
+    fichero.flush()
+    fichero.close()
 elif opcion == 3:
-    exit ()
+    exit()
 
 
 def pedirdatos():
